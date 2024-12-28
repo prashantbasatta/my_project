932f77b6c55fe88a1bef5e190c3f85c453e0436a
Prashantbasatta <your_email@example.com>
2024-12-28 20:55:16 +0530

    my project is successfull

---
+++ file_list.sh
@@ -0,0 +1,34 @@
+total 22800
+drwxrwxrwx 1 root root      512 Oct 21 12:17 AppData
+lrwxrwxrwx 1 root root       34 Oct 21 10:32 Application Data -> /mnt/c/Users/prash/AppData/Roaming
+drwxrwxrwx 1 root root      512 Oct 21 12:23 Contacts
+lrwxrwxrwx 1 root root       62 Oct 21 10:32 Cookies -> /mnt/c/Users/prash/AppData/Local/Microsoft/Windows/INetCookies
+drwxrwxrwx 1 root root      512 Dec 28 19:24 Downloads
+drwxrwxrwx 1 root root      512 Oct 21 12:23 Favorites
+drwxrwxrwx 1 root root      512 Dec 28 17:52 IntelGraphicsProfiles
+drwxrwxrwx 1 root root      512 Oct 21 12:23 Links
+lrwxrwxrwx 1 root root       32 Oct 21 10:32 Local Settings -> /mnt/c/Users/prash/AppData/Local
+drwxrwxrwx 1 root root      512 Oct 21 12:23 Music
+lrwxrwxrwx 1 root root       28 Oct 21 10:32 My Documents -> /mnt/c/Users/prash/Documents
+-rwxrwxrwx 1 root root 14942208 Dec 23 22:52 NTUSER.DAT
+-rwxrwxrwx 1 root root    65536 Oct 21 10:32 NTUSER.DAT{2ad838bc-efea-11ee-a54d-000d3a94eaa1}.TM.blf
+-rwxrwxrwx 1 root root   524288 Oct 21 10:32 NTUSER.DAT{2ad838bc-efea-11ee-a54d-000d3a94eaa1}.TMContainer00000000000000000001.regtrans-ms
+-rwxrwxrwx 1 root root   524288 Oct 21 10:32 NTUSER.DAT{2ad838bc-efea-11ee-a54d-000d3a94eaa1}.TMContainer00000000000000000002.regtrans-ms
+lrwxrwxrwx 1 root root       70 Oct 21 10:32 NetHood -> /mnt/c/Users/prash/AppData/Roaming/Microsoft/Windows/Network Shortcuts
+drwxrwxrwx 1 root root      512 Oct 21 12:17 OneDrive
+lrwxrwxrwx 1 root root       70 Oct 21 10:32 PrintHood -> /mnt/c/Users/prash/AppData/Roaming/Microsoft/Windows/Printer Shortcuts
+-rwxrwxrwx 1 root root     1445 Dec 28 11:39 README.md
+lrwxrwxrwx 1 root root       59 Oct 21 10:32 Recent -> /mnt/c/Users/prash/AppData/Roaming/Microsoft/Windows/Recent
+drwxrwxrwx 1 root root      512 Oct 21 12:23 Saved Games
+-rwxrwxrwx 1 root root    15910 Dec 28 11:39 Screenshot 2024-12-28 103840.png
+drwxrwxrwx 1 root root      512 Oct 21 12:23 Searches
+lrwxrwxrwx 1 root root       59 Oct 21 10:32 SendTo -> /mnt/c/Users/prash/AppData/Roaming/Microsoft/Windows/SendTo
+lrwxrwxrwx 1 root root       63 Oct 21 10:32 Start Menu -> /mnt/c/Users/prash/AppData/Roaming/Microsoft/Windows/Start Menu
+lrwxrwxrwx 1 root root       62 Oct 21 10:32 Templates -> /mnt/c/Users/prash/AppData/Roaming/Microsoft/Windows/Templates
+drwxrwxrwx 1 root root      512 Dec 28 16:43 Videos
+drwxrwxrwx 1 root root      512 Dec 28 19:43 app
+-rwxrwxrwx 1 root root      100 Dec 28 19:45 list.sh
+-rwxrwxrwx 1 root root     1067 Dec 28 10:54 monitor_performance.sh
+-rwxrwxrwx 1 root root  3646464 Oct 21 10:32 ntuser.dat.LOG1
+-rwxrwxrwx 1 root root  3613696 Oct 21 10:32 ntuser.dat.LOG2
+-rwxrwxrwx 1 root root       20 Oct 21 12:23 ntuser.ini
